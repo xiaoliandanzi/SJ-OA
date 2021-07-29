6c3122e14b5fd4f702aed93ae8e093e791de48c0
yanyizhi <1027354133@qq.com>
2021-07-29 16:28:46 +0800

    水印添加后缀名

--- src/main/java/com/active4j/hr/core/util/WaterMarkUtils.java
+++ src/main/java/com/active4j/hr/core/util/WaterMarkUtils.java
@@ -17,7 +17,7 @@ public class WaterMarkUtils {
      * @param waterMarkContent 水印内容
      * @param font 水印字体
      */
-    public void addWaterMark(String srcImgPath, String tarImgPath, String waterMarkContent,Font font) {
+    public void addWaterMark(String srcImgPath, String tarImgPath, String waterMarkContent,Font font,String suffix) {
 
         try {
             // 读取原图片信息
@@ -29,17 +29,17 @@ public class WaterMarkUtils {
             BufferedImage bufImg = new BufferedImage(srcImgWidth, srcImgHeight, BufferedImage.TYPE_INT_RGB);
             Graphics2D g = bufImg.createGraphics();
             g.drawImage(srcImg, 0, 0, srcImgWidth, srcImgHeight, null);
-            g.setColor(Color.red);; //根据图片的背景设置水印颜色
+            g.setColor(Color.WHITE);; //根据图片的背景设置水印颜色
             g.setFont(font);              //设置字体
 
             //设置水印的坐标
-            int x = srcImgWidth - getWatermarkLength(waterMarkContent, g);
-            int y = srcImgHeight - getWatermarkLength(waterMarkContent, g);
+            int x = srcImgWidth/2 - getWatermarkLength(waterMarkContent, g)/2;
+            int y = srcImgHeight - 10/* - getWatermarkLength(waterMarkContent, g)*/;
             g.drawString(waterMarkContent, x,y);  //画出水印
             g.dispose();
             // 输出图片
             FileOutputStream outImgStream = new FileOutputStream(tarImgPath);
-            ImageIO.write(bufImg, "png", outImgStream);
+            ImageIO.write(bufImg, suffix, outImgStream);
             System.out.println("添加水印完成");
             outImgStream.flush();
             outImgStream.close();
@@ -54,11 +54,11 @@ public class WaterMarkUtils {
 
 
     public static void main(String[] args) {
-        Font font = new Font("微软雅黑", Font.BOLD, 35);//水印字体
-        String srcImgPath="C:\\Users\\13353\\Desktop\\定时器.png"; //源图片地址
-        String tarImgPath="C:\\Users\\13353\\Desktop\\定时器123.png"; //待存储的地址
-        String waterMarkContent="我草尼玛，加你妈个比的傻逼标题水印";  //水印内容
-        new WaterMarkUtils().addWaterMark(srcImgPath, tarImgPath, waterMarkContent,font);
+        Font font = new Font("微软雅黑", Font.BOLD, 25);//水印字体
+        String srcImgPath="C:\\Users\\13353\\Desktop\\ceshi.jpg"; //源图片地址
+        String tarImgPath="C:\\Users\\13353\\Desktop\\ceshi123.jpg"; //待存储的地址
+        String waterMarkContent="测试测试测试测试，测试测试测试测试，测试测试测试测试，测试测试测试测试，测试测试测试测试，测试测试测试测试";  //水印内容
+        new WaterMarkUtils().addWaterMark(srcImgPath, tarImgPath, waterMarkContent,font,"jpg");
     }
 }
 
