b3b399971e2bc9f3a3dd3aa5a11d8100cffebb18
yanyizhi <1027354133@qq.com>
2021-07-29 16:52:55 +0800

    加static

--- src/main/java/com/active4j/hr/core/util/WaterMarkUtils.java
+++ src/main/java/com/active4j/hr/core/util/WaterMarkUtils.java
@@ -16,7 +16,7 @@ public class WaterMarkUtils {
      * @param tarImgPath 保存的图片路径
      * @param waterMarkContent 水印内容
      */
-    public void addWaterMark(String srcImgPath, String tarImgPath, String waterMarkContent,String suffix) {
+    public static void addWaterMark(String srcImgPath, String tarImgPath, String waterMarkContent,String suffix) {
 
         try {
             // 读取原图片信息
@@ -29,12 +29,12 @@ public class WaterMarkUtils {
             Graphics2D g = bufImg.createGraphics();
             g.drawImage(srcImg, 0, 0, srcImgWidth, srcImgHeight, null);
             g.setColor(Color.WHITE);; //根据图片的背景设置水印颜色
-            Font font = new Font("微软雅黑", Font.BOLD, 25);//水印字体
+            Font font = new Font("微软雅黑", Font.BOLD, 90);//水印字体
             g.setFont(font);              //设置字体
 
             //设置水印的坐标
             int x = srcImgWidth/2 - getWatermarkLength(waterMarkContent, g)/2;
-            int y = srcImgHeight - 10/* - getWatermarkLength(waterMarkContent, g)*/;
+            int y = srcImgHeight - 30/* - getWatermarkLength(waterMarkContent, g)*/;
             g.drawString(waterMarkContent, x,y);  //画出水印
             g.dispose();
             // 输出图片
@@ -48,7 +48,7 @@ public class WaterMarkUtils {
             System.out.println(e);
         }
     }
-    public int getWatermarkLength(String waterMarkContent, Graphics2D g) {
+    public static int getWatermarkLength(String waterMarkContent, Graphics2D g) {
         return g.getFontMetrics(g.getFont()).charsWidth(waterMarkContent.toCharArray(), 0, waterMarkContent.length());
     }
 
@@ -56,7 +56,7 @@ public class WaterMarkUtils {
     public static void main(String[] args) {
         String srcImgPath="C:\\Users\\13353\\Desktop\\ceshi.jpg"; //源图片地址
         String tarImgPath="C:\\Users\\13353\\Desktop\\ceshi123.jpg"; //待存储的地址
-        String waterMarkContent="测试测试测试测试，测试测试测试测试，测试测试测试测试，测试测试测试测试，测试测试测试测试，测试测试测试测试";  //水印内容
+        String waterMarkContent="测试测试测试测试测试，测试测试测试测试测试，测试测试测试测试...";  //水印内容
         new WaterMarkUtils().addWaterMark(srcImgPath, tarImgPath, waterMarkContent,"jpg");
     }
 }
